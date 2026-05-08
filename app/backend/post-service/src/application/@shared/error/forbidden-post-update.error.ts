b647690d2a84baa4eab581a327d5a0493cfc2c05
Pedro Reis <pedroreisalvess@gmail.com>
2026-05-08 11:27:34 -0300

    style: Fix all Prettier errors in the post-service codebase

--- app/backend/post-service/src/application/@shared/error/forbidden-post-update.error.ts
+++ app/backend/post-service/src/application/@shared/error/forbidden-post-update.error.ts
@@ -1,5 +1,8 @@
 export default class ForbiddenPostUpdateError extends Error {
-  constructor(public readonly postId: string, public readonly clientId: string) {
+  constructor(
+    public readonly postId: string,
+    public readonly clientId: string,
+  ) {
     super(`Client ${clientId} is not allowed to update post ${postId}`);
   }
 }
